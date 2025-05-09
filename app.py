0ba60165d2c3bc87436f53bf2d5b001171255f56
naveenkumar-299 <naveenlenin29@gmail.com>
2025-05-09 15:23:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+color-detection-app/
+│
+├── app.py                # Main application file
+├── templates/
+│   └── index.html        # HTML template for the homepage
+└── static/
+    └── uploads/          # Directory to store uploaded images
 
